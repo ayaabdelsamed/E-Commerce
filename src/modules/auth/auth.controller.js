1bd4a9657c0d2de974d0e2025c090b287ba0df6d
Aya Abdelsamed <aya.abdelsamed11pp@gmail.com>
2025-02-02 17:37:19 +0200

    Change User Password

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -10,7 +10,7 @@ const signup =catchError(async(req,res,next)=>{
     await user.save()
     let token = jwt.sign({userId:user._id,email:user.email},'aykey')
     res.json({message:"success",token})
-    
+     
 })
 
 
@@ -27,6 +27,23 @@ const signin =catchError(async(req,res,next)=>{
     next(new AppError("incorrect mail or password",401))
 })
 
+const changeUserPassword =catchError(async(req,res,next)=>{
+    let user = await userModel.findOne({email:req.body.email})
+
+    if(user&&bcrypt.compareSync(req.body.oldPassword,user.password)){
+        
+        await userModel.findOneAndUpdate({email:req.body.email},{password:req.body.newPassword})
+        let token = jwt.sign({userId:user._id,email:user.email},'aykey')
+       // if(user.verifyEmail)
+        return res.json({message:"success",token})
+        // else  
+        //     return next(new AppError("verify email first",401))
+    }
+    //return res.json({message:"incorrect mail or password"})
+    next(new AppError("incorrect mail or password",401))
+})
+
+
 // const verify = catchError(async(req,res,next)=>{
 //     jwt.verify(req.params.token,process.env.JWT_KEY,async(err,decoded)=>{
 //         if(err) return next(new AppError(err,401))
@@ -39,5 +56,6 @@ const signin =catchError(async(req,res,next)=>{
 
 export{
     signup,
-    signin
+    signin,
+    changeUserPassword
 }
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { signin, signup } from './auth.controller.js'
+import { changeUserPassword, signin, signup } from './auth.controller.js'
 import { checkEmail } from '../../middleware/checkEmail.js'
 
 const authRouter=express.Router()
 
 authRouter.post('/signup',checkEmail,signup)
 authRouter.post('/signin',signin)
+authRouter.patch('/change-password',changeUserPassword)
 //authRouter.get('/verify/:token',verify)
 
 export default authRouter
